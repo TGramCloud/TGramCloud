2ac4cfbe4ec8244e75faed5a6d24d0167eba19c1
Inspector Soviet <www.alex.avil@mail.ru>
2023-01-14 14:47:12 +0400

    commentary and owner tracking

--- engine.js
+++ engine.js
@@ -1,8 +1,10 @@
+//Required dependencies
 let EngineDependencies = {
   TelegramBot: require("node-telegram-bot-api"),
   sql: require("better-sqlite3")
 }
 
+//Variables used by the engine
 let EngineVariables = {
   DefaultLang: "en",
   Token: process.env.TOKEN || process.argv[2],
@@ -10,7 +12,9 @@ let EngineVariables = {
   Instance: {}
 };
 
+//Functions used by the engine
 let EngineFunctions = {
+  //Init instance to allow easy access to the engine
   InitInstance: function (DBName) {
     instance = {
       bot: new EngineDependencies.TelegramBot(EngineVariables.Token, {
@@ -19,11 +23,13 @@ let EngineFunctions = {
       }),
       db: new EngineDependencies.sql(DBName),
       pm2: process.env.PM2_HOME !== undefined,
-      child: require("child_process")
+      child: require("child_process"),
+      owner: EngineVariables.OwnerID,
     };
     EngineVariables.Instance = instance;
   },
 
+  //Create a settings table with stock settings
   CreateSettingsTable: function () {
     let db = EngineVariables.Instance.db;
     db.prepare(
@@ -37,6 +43,7 @@ let EngineFunctions = {
     ).run();
   },
 
+  //Add a new setting to the settings table
   AddSetting: function (option, value) {
     let db = EngineVariables.Instance.db;
     db.prepare(
@@ -44,13 +51,14 @@ let EngineFunctions = {
     ).run(option, value);
   },
 
+  //Get a setting from the settings table
   GetSetting: function (option) {
     let db = EngineVariables.Instance.db;
-    return db
-      .prepare("SELECT value FROM settings WHERE option = ?")
-      .get(option).value;
+    return db.prepare("SELECT value FROM settings WHERE option = ?").get(option)
+      .value;
   },
 
+  //Set a new value of a setting
   SetSetting: function (option, value) {
     let db = EngineVariables.Instance.db;
     db.prepare("UPDATE settings SET value = ? WHERE option = ?").run(
@@ -59,22 +67,22 @@ let EngineFunctions = {
     );
   },
 
+  //Delete the setting
   RemoveSetting: function (option) {
     let db = EngineVariables.Instance.db;
     db.prepare("DELETE FROM settings WHERE option = ?").run(option);
   },
 
+  //Reset settings to defaults
   ResetSettings: function () {
     let db = EngineVariables.Instance.db;
     db.prepare("DELETE FROM settings").run();
   },
 
+  //Check if a bot is run for the first time or if the settings are reset
   CheckFirstRun: function () {
     let db = EngineVariables.Instance.db;
-    switch (
-      db.prepare("SELECT value FROM settings WHERE option = 'first_run'").get()
-        .value
-    ) {
+    switch (this.GetSetting("first_run")) {
       case "true":
         return true;
       case "false":
@@ -84,6 +92,8 @@ let EngineFunctions = {
     }
   },
 
+  //Create a users table with stock parameters
+  //You can override the default parameters by adding your own function in the custom file
   CreateUsersTable: function () {
     let db = EngineVariables.Instance.db;
     db.prepare(
@@ -91,11 +101,14 @@ let EngineFunctions = {
     ).run();
   },
 
+  //Wipe all users
   WipeUsersTable: function () {
     let db = EngineVariables.Instance.db;
     db.prepare("DELETE FROM users").run();
   },
 
+  //Add a new user to the users table
+  //You can override the default parameters by adding your own function in the custom file
   AddUser: function (id, status, lang) {
     let db = EngineVariables.Instance.db;
     db.prepare(
@@ -103,108 +116,146 @@ let EngineFunctions = {
     ).run(id, status, lang);
   },
 
+  //Get a user from the users table
   GetUser: function (id) {
     let db = EngineVariables.Instance.db;
-    return db
-      .prepare("SELECT * FROM users WHERE id = ?")
-      .get(id);
+    return db.prepare("SELECT * FROM users WHERE id = ?").get(id);
   },
 
+  //Delete a user from the users table
   DeleteUser: function (id) {
     let db = EngineVariables.Instance.db;
     db.prepare("DELETE FROM users WHERE id = ?").run(id);
   },
 
+  //Edit a user's data
+  //You can override the default parameters by adding your own function in the custom file
   EditUser: function (id, status, lang) {
     let db = EngineVariables.Instance.db;
-    db.prepare(
-      "UPDATE users SET status = ?, lang = ? WHERE id = ?"
-    ).run(status, lang, id);
+    db.prepare("UPDATE users SET status = ?, lang = ? WHERE id = ?").run(
+      status,
+      lang,
+      id
+    );
   },
 
-  
+  //Set the current version of the bot
   SetCurrentVersion: function () {
-    EngineVariables.Instance.child.exec("git fetch -q && git ls-remote --heads --quiet", (err, stdout, stderr) => {
-      if (err) {
-        console.log(err);
-      } else {
-        this.SetSetting("current_version", stdout.toString().substring(0, 7));
+    EngineVariables.Instance.child.exec(
+      "git fetch -q && git ls-remote --heads --quiet",
+      (err, stdout, stderr) => {
+        if (err) {
+          console.log(err);
+        } else {
+          this.SetSetting("current_version", stdout.toString().substring(0, 7));
+        }
       }
-    })
+    );
   },
 
+  //Get the current version of the bot
   GetCurrentVersion: function () {
     return this.GetSetting("current_version");
   },
 
+  //Bot updater routine
   UpdateBot: function (msg) {
-    EngineVariables.Instance.child.exec("git fetch -q && git ls-remote --heads --quiet", (err, stdout, stderr) => {
-      if (err) {
-        EngineVariables.Instance.bot.sendMessage(msg.chat.id, "Error while fetching updates.");
-      } else {
-        console.log(stdout);
-        if (stdout.toString().substring(0, 7) != this.GetSetting("current_version")) {
-          EngineVariables.Instance.bot.sendMessage(msg.chat.id, "Are you sure you want to update? (y/n)", {
-            reply_markup: {
-              inline_keyboard: [
-                [
-                  {
-                    text: "Yes",
-                    callback_data: "yes",
-                  },
-                ],
-                [
-                  {
-                    text: "No",
-                    callback_data: "no",
-                  },
-                ],
-              ],
-            },
-          });
-          EngineVariables.Instance.bot.once("callback_query", (callback) => {
-            switch (callback.data) {
-              case "yes":
-                //Update the bot
-                EngineVariables.Instance.bot.sendMessage(msg.chat.id, "Downloading update...");
-                //Execute the update script
-                EngineVariables.Instance.child.exec(
-                  "./update.sh",
-                  function(err, stdout, stderr) {
-                    if (err) {
-                      console.log(err);
-                      EngineVariables.Instance.bot.sendMessage(msg.chat.id, "Error while installing updates.");
-                    } else {
-                      console.log(stdout);
-                      child.exec("chmod 777 ./update.sh");
-                      EngineFunctions.RestartInstance();
-                      EngineVariables.Instance.bot.sendMessage(msg.chat.id, "Finished updating.");
-                      EngineFunctions.SetCurrentVersion();
+    EngineVariables.Instance.child.exec(
+      "git fetch -q && git ls-remote --heads --quiet",
+      (err, stdout, stderr) => {
+        if (err) {
+          EngineVariables.Instance.bot.sendMessage(
+            msg.chat.id,
+            "Error while fetching updates."
+          );
+        } else {
+          console.log(stdout);
+          if (
+            stdout.toString().substring(0, 7) !=
+            this.GetSetting("current_version")
+          ) {
+            EngineVariables.Instance.bot.sendMessage(
+              msg.chat.id,
+              "Are you sure you want to update? (y/n)",
+              {
+                reply_markup: {
+                  inline_keyboard: [
+                    [
+                      {
+                        text: "Yes",
+                        callback_data: "yes",
+                      },
+                    ],
+                    [
+                      {
+                        text: "No",
+                        callback_data: "no",
+                      },
+                    ],
+                  ],
+                },
+              }
+            );
+            EngineVariables.Instance.bot.once("callback_query", (callback) => {
+              switch (callback.data) {
+                case "yes":
+                  //Update the bot
+                  EngineVariables.Instance.bot.sendMessage(
+                    msg.chat.id,
+                    "Downloading update..."
+                  );
+                  //Execute the update script
+                  EngineVariables.Instance.child.exec(
+                    "./update.sh",
+                    function (err, stdout, stderr) {
+                      if (err) {
+                        console.log(err);
+                        EngineVariables.Instance.bot.sendMessage(
+                          msg.chat.id,
+                          "Error while installing updates."
+                        );
+                      } else {
+                        console.log(stdout);
+                        child.exec("chmod 777 ./update.sh");
+                        EngineFunctions.RestartInstance();
+                        EngineVariables.Instance.bot.sendMessage(
+                          msg.chat.id,
+                          "Finished updating."
+                        );
+                        EngineFunctions.SetCurrentVersion();
+                      }
                     }
-                  }
-                );
-                break;
-              case "no":
-                EngineVariables.Instance.bot.sendMessage(msg.chat.id, "Cancelled.");
-                break;
-            }
-          });
-        }
-        else {
-          EngineVariables.Instance.bot.sendMessage(msg.chat.id, "No updates found!");
+                  );
+                  break;
+                case "no":
+                  EngineVariables.Instance.bot.sendMessage(
+                    msg.chat.id,
+                    "Cancelled."
+                  );
+                  break;
+              }
+            });
+          } else {
+            EngineVariables.Instance.bot.sendMessage(
+              msg.chat.id,
+              "No updates found!"
+            );
+          }
         }
       }
-    }
     );
   },
 
+  //Restart the bot
   RestartInstance: function () {
     if (EngineVariables.Instance.pm2) {
       const pm2 = require("pm2");
-      pm2.restart("./index.js")
+      pm2.restart("./index.js");
     }
-  }
-}
+  },
+
+
+};
 
 module.exports = {
   EngineDependencies,
