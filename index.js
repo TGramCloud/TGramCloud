ab3df966442ee121feca354c31f305d617e45c65
Inspector Soviet <www.alex.avil@mail.ru>
2023-02-28 12:37:36 +0400

    add sentry support

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const {
   EngineVariables,
 } = require("./engine.js");
 const { CustomFunctions } = require("./functions.js");
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://d249f99c4bd745f0bf8877b01395bedd@o4504756755824640.ingest.sentry.io/4504756756873216",
+  tracesSampleRate: 1.0,
+});
 
 var adminid = "";
 
@@ -29,5 +35,16 @@ EngineVariables.Instance.bot.onText(/\/update/, (msg) => {
 });
 
 EngineVariables.Instance.bot.on("polling_error", (err) => {
+  Sentry.captureException(err);
   console.log(err);
+});
+
+process.on("unhandledRejection", (error) => {
+  Sentry.captureException(error);
+  console.error("Error:", error);
+});
+
+process.on("uncaughtException", (error) => {
+  Sentry.captureException(error);
+  console.error("Error:", error);
 });
